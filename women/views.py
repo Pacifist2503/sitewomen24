b3a5a60121ed8263d3168994326d8168e123b4e4
NTOLI <89056670385@ya.ru>
2024-01-09 10:50:07 +0300

    GET- и POST-запросы. Обработчики исключений запросов

--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -11,8 +11,14 @@ def categories(request, cat_id):
 
 
 def year_archive(request, year):
+    if year > 2024:
+        raise Http404()
     return HttpResponse(f"<h1>Статьи по категориям</h1> Год {year}")
 
 
 def categories_slug(request, cat_slug):
     return HttpResponse(f"<h1>Статьи по категориям</h1> SLUG {cat_slug}")
+
+
+def handler404(request, exception):
+    return HttpResponseNotFound('Страница не найдена')
