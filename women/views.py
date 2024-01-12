e9c69b5b86a8efe5e8be3269415ec928ca9e57ea
NTOLI <89056670385@ya.ru>
2024-01-12 10:21:10 +0300

    Теги шаблонов. Теги if и for

--- women/views.py
+++ women/views.py
@@ -4,16 +4,25 @@ from django.urls import reverse
 
 menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+    {'id': 4, 'title': 'Пугачева', 'content': 'Пугачева', 'is_published': True},
+]
+
 
 def index(request):
     data = {'menu': menu,
-            'title': 'Главная страница'}
+            'title': 'Главная страница',
+            'posts': data_db}
     return render(request, 'women/index.html', context=data)
 
 
 def about(request):
     data = {'menu': menu,
-            'title': 'О сайте'}
+            'title': 'О сайте',
+            'posts': data_db}
     return render(request, 'women/about.html', context=data)
 
 
