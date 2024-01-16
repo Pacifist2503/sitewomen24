c409496fea082653458402a30896bb57113d659d
NTOLI <89056670385@ya.ru>
2024-01-16 12:59:25 +0300

    Добавляем связь many-to-many (многие ко многим)

--- women/models.py
+++ women/models.py
@@ -19,6 +19,7 @@ class Women(models.Model):
     time_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
     cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts_cat')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='posts_tag')
 
     published = PublishedManager()
     objects = models.Manager()
@@ -45,3 +46,11 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_slug': self.slug})
+
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
