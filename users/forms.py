29f000ce8fca5ee894cce3a4f63da47258b721b2
NTOLI <89056670385@ya.ru>
2024-02-06 13:01:07 +0300

    Классы LoginView, LogoutView и AuthenticationForm

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,29 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label='Логин',
                                widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'имя'}))
     password = forms.CharField(label='Пароль',
                                widget=forms.PasswordInput(attrs={'class': 'input'}))
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password')
+
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label='Логин')
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name', 'password')
+        labels = {
+            'email': 'E-mail',
+            'first_name': 'Имя',
+            'last_name': 'Фамилия'
+        }
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users import views
@@ -5,6 +6,6 @@ from users import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.LoginUser.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
